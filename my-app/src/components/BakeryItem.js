8e61844582d950f643f48cb1af10fa6908de1511
Sandra Martinez <sandra_martinez@brown.edu--global user.name smarti86>
2024-04-07 22:45:37 -0400

    still styling

--- my-app/src/components/BakeryItem.js
+++ my-app/src/components/BakeryItem.js
@@ -14,7 +14,7 @@ export default function TeeItem({item, addToCart }) {
                         <h2 className="bakery-item-label">{item.name}</h2>
                         <p className="bakery-item-description">{item.size}</p>
                         <p className="bakery-item-description">{item.color}</p>
-                        <p className="price">${item.price}</p>
+                        <p className="bakery-item-description">${item.price}</p>
                         <button onClick={handleAddToCart} className="button">Add to Cart</button>
                     </div>
                 </div>
